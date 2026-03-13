ddfffb2ebc61820018735f07a95d3ae84fddfd38
AnupamKumar9595 <kumaranupam762@gmail.com>
2026-03-13 15:23:25 +0530

    Add security dependencies and implement JWT authentication
    - Added spring-boot-starter-security for security features.
    - Introduced JJWT for JWT handling.
    - Created Authority, JwtRequest, and JwtResponse models for authentication.
    - Updated User model to implement UserDetails and manage authorities.
    - Modified UserController to allow cross-origin requests.
    - Updated application.properties for database connection settings.

--- src/main/java/com/exam/ExamserverApplication.java
+++ src/main/java/com/exam/ExamserverApplication.java
@@ -1,16 +1,10 @@
 package com.exam;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.exam.model.Role;
-import com.exam.model.User;
-import com.exam.model.UserRole;
 import com.exam.service.UserService;
 
 @SpringBootApplication
--- src/main/java/com/exam/model/User.java
+++ src/main/java/com/exam/model/User.java
@@ -1,7 +1,11 @@
 package com.exam.model;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -16,7 +20,7 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails{
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
@@ -125,6 +129,21 @@ public class User {
     public void setUserRoles(Set<UserRole> userRoles) {
         this.userRoles = userRoles;
     }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // TODO Auto-generated method stub
+
+        Set<Authority> set = new HashSet<>();
+
+        this.userRoles.forEach(userRole -> {
+            set.add(new Authority(userRole.getRole().getRoleName()));
+        });
+
+        return set;
+    }
+
+    
 
     
 }
